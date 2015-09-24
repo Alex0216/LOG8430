fa74b3e635a98856e1143ddd6b711fb7f53d6f05
unknown <mathieu.laprise@polymtl.ca>
2015-09-24 16:09:30 -0400

    Executer.java

--- LOG8430 - TP1/src/ca/polymtl/log8430/tp1/Controller/Executer.java
+++ LOG8430 - TP1/src/ca/polymtl/log8430/tp1/Controller/Executer.java
@@ -1,5 +1,14 @@
 package ca.polymtl.log8430.tp1.Controller;
+import java.util.ArrayList;
+import ca.polymtl.log8430.tp1.Model.*;
 
 public class Executer {
+	public int executeCommand(Command command) 
+	{
+		return 0;
+		
+	}
+	
+	ArrayList<Command> m_listCommand;
 
 }
